e6dae6d9959cc60db6776a3fea62c453f17673a0
Bryan Paredes <bryansparedes2@gmail.com>
2024-10-07 21:36:53 -0600

    routes added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import tasksRoutes from "./routes/tasks.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+//! Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//! Routes
+app.use("/api", tasksRoutes);
+app.use("/api", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Wlelcome to my API" });
 });
 
+//! Error Handler
 app.use((err, req, res, next) => {
   res.status(500).json({
     status: "error",
