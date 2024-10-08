a224e6b8d41d9ab070e310d32b690f7c670b3553
Bryan Paredes <bryansparedes2@gmail.com>
2024-10-07 22:49:00 -0600

    Create db inster into task.controllers to create a task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,8 +1,25 @@
+import { pool } from "../db.js";
+
 export const getAllTasks = (req, res) => res.send("Obteniendo tareas");
 
 export const getTask = (req, res) => res.send("Obteniendo tarea unica");
 
-export const createTask = (req, res) => res.send("Creando tarea");
+export const createTask = async (req, res, next) => {
+  const { title, description } = req.body;
+  //db insert
+  try {
+    const result = await pool.query(
+      "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.send("Task already exists");
+    }
+    next(error);
+  }
+};
 
 export const updateTask = (req, res) => res.send("Actualizando tarea unica");
 
