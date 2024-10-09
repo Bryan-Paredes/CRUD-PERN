a40d6cb85c48112cc53f58b475fa09c54e3b086d
Bryan Paredes <bryansparedes2@gmail.com>
2024-10-08 21:31:50 -0600

    creating signin function requesting email and comparing encrypted password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,27 @@ import { pool } from "../db.js";
 import bcrypt from "bcrypt";
 import { createAccessToken } from "../lib/jwt.js";
 
-export const signin = (req, res) => res.send("Ingresando");
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (result.rowCount === 0) {
+    return res.status(400).json({ message: "Usuario no encontrado" });
+  }
+  const validPassword = await bcrypt.compare(password, result.rows[0].password);
+  if (!validPassword) {
+    return res.status(400).json({ message: "Contraseña incorrecta" });
+  }
+  const token = await createAccessToken({ id: result.rows[0].id });
+  res.cookie("token", token, {
+    httpOnly: true,
+    //   secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  });
+  return res.json(result.rows[0]);
+};
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
