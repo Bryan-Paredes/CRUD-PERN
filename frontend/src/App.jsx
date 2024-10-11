3021691928b4deab40875552ac4cb2cca6788218
Bryan Paredes <bryansparedes2@gmail.com>
2024-10-10 18:07:53 -0600

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import TasksPage from "./pages/TasksPage";
+import TaskFromPage from "./pages/TaskFromPage";
+import AboutPage from "./pages/AboutPage";
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/about" element={<AboutPage />} />
+
+      <Route path="/tasks" element={<TasksPage />} />
+      <Route path="/tasks/new" element={<TaskFromPage />} />
+      <Route path="/tasks/1/edit" element={<TaskFromPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  );
 }
---
+++ frontend/src/pages/TaskFromPage.jsx
@@ -0,0 +1,3 @@
+export default function TaskFromPage() {
+  return <div>TaskFromPage</div>;
+}
